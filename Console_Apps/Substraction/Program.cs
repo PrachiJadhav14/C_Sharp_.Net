cb08800fe445e5d294661b0b8c8c04801cb1a196
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-07 00:12:26 +0530

    Substraction

---
+++ Console_Apps/Substraction/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Substraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No1 = 0, No2 = 0, Res = 0;
+            Console.Write("\n Enter First No => ");
+            No1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n Enter Second No => ");
+            No2 = Convert.ToInt32(Console.ReadLine());
+
+            Res = No1 - No2;
+
+            Console.Write("\n Substraction is " + No1 + " & " + No2 + " is " + Res);
+
+            Console.ReadKey();
+        }
+    }
+}
