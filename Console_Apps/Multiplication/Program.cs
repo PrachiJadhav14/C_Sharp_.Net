e97b1b755cebe8c9a747b161c5d1865fefe8c4ab
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-07 00:10:40 +0530

    Multiplication

---
+++ Console_Apps/Multiplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No1 = 0, No2 = 0, Res = 0;
+
+            Console.Write("\n Enter First No => ");
+            No1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("\n Enter Second No => ");
+            No2 = Convert.ToInt32(Console.ReadLine());
+
+            Res = No1 * No2;
+
+            Console.Write("\n Multiplication is " + No1 + " & " + No2 + " is " + Res);
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
