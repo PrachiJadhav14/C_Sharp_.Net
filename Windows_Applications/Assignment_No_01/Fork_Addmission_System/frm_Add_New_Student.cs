2ccecf247b70e08fc8a94268ecabdb7bc2e06856
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-09 23:25:39 +0530

    Assignment_no_1

---
+++ Windows_Applications/Assignment_No_01/Fork_Addmission_System/frm_Add_New_Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fork_Addmission_System
+{
+    public partial class frm_Add_New_Student : Form
+    {
+        public frm_Add_New_Student()
+        {
+            InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void frm_Add_New_Student_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Roll_No_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_Roll_No_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_Mob_No_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmb_Date_Of_Birth_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmb_Course_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Student_List_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Save_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
