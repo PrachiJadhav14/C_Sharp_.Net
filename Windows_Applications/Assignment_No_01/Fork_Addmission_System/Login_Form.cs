cdaab0bf79439c8e75ffde34f472d7abaeeec73a
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-09 23:19:01 +0530

    Assignment_No_1

---
+++ Windows_Applications/Assignment_No_01/Fork_Addmission_System/Login_Form.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fork_Addmission_System
+{
+    public partial class frm_Login : Form
+    {
+        public frm_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_Login_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Login_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lbl_Login_Form_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_UserName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
