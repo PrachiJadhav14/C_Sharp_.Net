172cebc76edd1344025a9c339055070b34a1a05c
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-09 23:26:09 +0530

    Assignment_no_1

---
+++ Windows_Applications/Assignment_No_01/Fork_Addmission_System/frm_Add_New_Student.Designer.cs
@@ -0,0 +1,299 @@
+﻿
+namespace Fork_Addmission_System
+{
+    partial class frm_Add_New_Student
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnl_Header = new System.Windows.Forms.Panel();
+            this.btn_Logout = new System.Windows.Forms.Button();
+            this.lbl_Add_Student_Details = new System.Windows.Forms.Label();
+            this.lbl_Roll_No = new System.Windows.Forms.Label();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_Date_OF_Birth = new System.Windows.Forms.Label();
+            this.lbl_Mob_No = new System.Windows.Forms.Label();
+            this.lbl_Course = new System.Windows.Forms.Label();
+            this.tb_Roll_No = new System.Windows.Forms.TextBox();
+            this.tb_Name = new System.Windows.Forms.TextBox();
+            this.cmb_Date_Of_Birth = new System.Windows.Forms.DateTimePicker();
+            this.tb_Mob_No = new System.Windows.Forms.TextBox();
+            this.cmb_Course = new System.Windows.Forms.ComboBox();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.btn_Student_List = new System.Windows.Forms.Button();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.pnl_Header.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnl_Header
+            // 
+            this.pnl_Header.Controls.Add(this.btn_Logout);
+            this.pnl_Header.Controls.Add(this.lbl_Add_Student_Details);
+            this.pnl_Header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnl_Header.Location = new System.Drawing.Point(0, 0);
+            this.pnl_Header.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.pnl_Header.Name = "pnl_Header";
+            this.pnl_Header.Size = new System.Drawing.Size(894, 117);
+            this.pnl_Header.TabIndex = 0;
+            this.pnl_Header.Tag = "";
+            this.pnl_Header.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // btn_Logout
+            // 
+            this.btn_Logout.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btn_Logout.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Logout.ForeColor = System.Drawing.Color.Red;
+            this.btn_Logout.Location = new System.Drawing.Point(742, 30);
+            this.btn_Logout.Name = "btn_Logout";
+            this.btn_Logout.Size = new System.Drawing.Size(118, 42);
+            this.btn_Logout.TabIndex = 9;
+            this.btn_Logout.Text = "LogOut";
+            this.btn_Logout.UseVisualStyleBackColor = false;
+            // 
+            // lbl_Add_Student_Details
+            // 
+            this.lbl_Add_Student_Details.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_Add_Student_Details.AutoSize = true;
+            this.lbl_Add_Student_Details.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lbl_Add_Student_Details.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Add_Student_Details.Font = new System.Drawing.Font("Modern No. 20", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Add_Student_Details.Location = new System.Drawing.Point(248, 30);
+            this.lbl_Add_Student_Details.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lbl_Add_Student_Details.Name = "lbl_Add_Student_Details";
+            this.lbl_Add_Student_Details.Size = new System.Drawing.Size(484, 60);
+            this.lbl_Add_Student_Details.TabIndex = 0;
+            this.lbl_Add_Student_Details.Text = "Add Student Details";
+            this.lbl_Add_Student_Details.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lbl_Roll_No
+            // 
+            this.lbl_Roll_No.AutoSize = true;
+            this.lbl_Roll_No.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Roll_No.Location = new System.Drawing.Point(148, 118);
+            this.lbl_Roll_No.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.lbl_Roll_No.Name = "lbl_Roll_No";
+            this.lbl_Roll_No.Size = new System.Drawing.Size(116, 34);
+            this.lbl_Roll_No.TabIndex = 1;
+            this.lbl_Roll_No.Text = "Roll No";
+            this.lbl_Roll_No.Click += new System.EventHandler(this.lbl_Roll_No_Click);
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Name.Location = new System.Drawing.Point(148, 198);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(89, 34);
+            this.lbl_Name.TabIndex = 3;
+            this.lbl_Name.Text = "Name";
+            // 
+            // lbl_Date_OF_Birth
+            // 
+            this.lbl_Date_OF_Birth.AutoSize = true;
+            this.lbl_Date_OF_Birth.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Date_OF_Birth.Location = new System.Drawing.Point(148, 275);
+            this.lbl_Date_OF_Birth.Name = "lbl_Date_OF_Birth";
+            this.lbl_Date_OF_Birth.Size = new System.Drawing.Size(199, 34);
+            this.lbl_Date_OF_Birth.TabIndex = 4;
+            this.lbl_Date_OF_Birth.Text = "Date Of Birth";
+            // 
+            // lbl_Mob_No
+            // 
+            this.lbl_Mob_No.AutoSize = true;
+            this.lbl_Mob_No.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Mob_No.Location = new System.Drawing.Point(148, 361);
+            this.lbl_Mob_No.Name = "lbl_Mob_No";
+            this.lbl_Mob_No.Size = new System.Drawing.Size(116, 34);
+            this.lbl_Mob_No.TabIndex = 5;
+            this.lbl_Mob_No.Text = "Mob No";
+            // 
+            // lbl_Course
+            // 
+            this.lbl_Course.AutoSize = true;
+            this.lbl_Course.Font = new System.Drawing.Font("Modern No. 20", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Course.Location = new System.Drawing.Point(148, 440);
+            this.lbl_Course.Name = "lbl_Course";
+            this.lbl_Course.Size = new System.Drawing.Size(101, 34);
+            this.lbl_Course.TabIndex = 6;
+            this.lbl_Course.Text = "Course";
+            // 
+            // tb_Roll_No
+            // 
+            this.tb_Roll_No.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Roll_No.Location = new System.Drawing.Point(442, 118);
+            this.tb_Roll_No.MaxLength = 8;
+            this.tb_Roll_No.Name = "tb_Roll_No";
+            this.tb_Roll_No.Size = new System.Drawing.Size(290, 37);
+            this.tb_Roll_No.TabIndex = 1;
+            this.tb_Roll_No.TextChanged += new System.EventHandler(this.tb_Roll_No_TextChanged);
+            // 
+            // tb_Name
+            // 
+            this.tb_Name.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Name.Location = new System.Drawing.Point(442, 195);
+            this.tb_Name.MaxLength = 20;
+            this.tb_Name.Name = "tb_Name";
+            this.tb_Name.Size = new System.Drawing.Size(290, 37);
+            this.tb_Name.TabIndex = 2;
+            // 
+            // cmb_Date_Of_Birth
+            // 
+            this.cmb_Date_Of_Birth.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Date_Of_Birth.Location = new System.Drawing.Point(442, 275);
+            this.cmb_Date_Of_Birth.MaxDate = new System.DateTime(2025, 6, 1, 0, 0, 0, 0);
+            this.cmb_Date_Of_Birth.MinDate = new System.DateTime(1997, 6, 1, 0, 0, 0, 0);
+            this.cmb_Date_Of_Birth.Name = "cmb_Date_Of_Birth";
+            this.cmb_Date_Of_Birth.Size = new System.Drawing.Size(290, 37);
+            this.cmb_Date_Of_Birth.TabIndex = 3;
+            this.cmb_Date_Of_Birth.Value = new System.DateTime(2025, 6, 1, 0, 0, 0, 0);
+            this.cmb_Date_Of_Birth.ValueChanged += new System.EventHandler(this.cmb_Date_Of_Birth_ValueChanged);
+            // 
+            // tb_Mob_No
+            // 
+            this.tb_Mob_No.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_Mob_No.Location = new System.Drawing.Point(442, 358);
+            this.tb_Mob_No.MaxLength = 10;
+            this.tb_Mob_No.Name = "tb_Mob_No";
+            this.tb_Mob_No.Size = new System.Drawing.Size(290, 37);
+            this.tb_Mob_No.TabIndex = 4;
+            this.tb_Mob_No.TextChanged += new System.EventHandler(this.tb_Mob_No_TextChanged);
+            // 
+            // cmb_Course
+            // 
+            this.cmb_Course.Font = new System.Drawing.Font("Modern No. 20", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmb_Course.FormattingEnabled = true;
+            this.cmb_Course.Items.AddRange(new object[] {
+            "MSC(CS)",
+            "MCS",
+            "MCA",
+            "BCOM (IT)",
+            "BSC(CS)",
+            "BCS",
+            "BCA",
+            "",
+            "",
+            "",
+            "",
+            ""});
+            this.cmb_Course.Location = new System.Drawing.Point(442, 440);
+            this.cmb_Course.MaxDropDownItems = 6;
+            this.cmb_Course.Name = "cmb_Course";
+            this.cmb_Course.Size = new System.Drawing.Size(290, 38);
+            this.cmb_Course.TabIndex = 5;
+            this.cmb_Course.SelectedIndexChanged += new System.EventHandler(this.cmb_Course_SelectedIndexChanged);
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btn_Refresh.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Refresh.ForeColor = System.Drawing.Color.Red;
+            this.btn_Refresh.Location = new System.Drawing.Point(38, 525);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(159, 49);
+            this.btn_Refresh.TabIndex = 6;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = false;
+            // 
+            // btn_Student_List
+            // 
+            this.btn_Student_List.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btn_Student_List.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Student_List.ForeColor = System.Drawing.Color.Red;
+            this.btn_Student_List.Location = new System.Drawing.Point(593, 525);
+            this.btn_Student_List.Name = "btn_Student_List";
+            this.btn_Student_List.Size = new System.Drawing.Size(241, 49);
+            this.btn_Student_List.TabIndex = 8;
+            this.btn_Student_List.Text = "Student List";
+            this.btn_Student_List.UseVisualStyleBackColor = false;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.btn_Save.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Save.ForeColor = System.Drawing.Color.Red;
+            this.btn_Save.Location = new System.Drawing.Point(328, 525);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(159, 49);
+            this.btn_Save.TabIndex = 7;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = false;
+            // 
+            // frm_Add_New_Student
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(894, 606);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.btn_Student_List);
+            this.Controls.Add(this.btn_Refresh);
+            this.Controls.Add(this.cmb_Course);
+            this.Controls.Add(this.tb_Mob_No);
+            this.Controls.Add(this.cmb_Date_Of_Birth);
+            this.Controls.Add(this.tb_Name);
+            this.Controls.Add(this.tb_Roll_No);
+            this.Controls.Add(this.lbl_Course);
+            this.Controls.Add(this.lbl_Mob_No);
+            this.Controls.Add(this.lbl_Date_OF_Birth);
+            this.Controls.Add(this.lbl_Name);
+            this.Controls.Add(this.lbl_Roll_No);
+            this.Controls.Add(this.pnl_Header);
+            this.Font = new System.Drawing.Font("Modern No. 20", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frm_Add_New_Student";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Add New Student";
+            this.Load += new System.EventHandler(this.frm_Add_New_Student_Load);
+            this.pnl_Header.ResumeLayout(false);
+            this.pnl_Header.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_Header;
+        private System.Windows.Forms.Label lbl_Add_Student_Details;
+        private System.Windows.Forms.Label lbl_Roll_No;
+        private System.Windows.Forms.Label lbl_Name;
+        private System.Windows.Forms.Label lbl_Date_OF_Birth;
+        private System.Windows.Forms.Label lbl_Mob_No;
+        private System.Windows.Forms.Label lbl_Course;
+        private System.Windows.Forms.TextBox tb_Roll_No;
+        private System.Windows.Forms.TextBox tb_Name;
+        private System.Windows.Forms.TextBox tb_Mob_No;
+        private System.Windows.Forms.ComboBox cmb_Course;
+        private System.Windows.Forms.DateTimePicker cmb_Date_Of_Birth;
+        private System.Windows.Forms.Button btn_Refresh;
+        private System.Windows.Forms.Button btn_Student_List;
+        private System.Windows.Forms.Button btn_Save;
+        private System.Windows.Forms.Button btn_Logout;
+    }
+}
