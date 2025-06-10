b435c49cf83b0e6cb8308ab0f0d214e269df03f4
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-10 16:57:24 +0530

    Assignment_no_1

---
+++ Windows_Applications/Assignment_No_01/Fork_Addmission_System/frm_Student_List.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fork_Addmission_System
+{
+    public partial class frm_Student_List : Form
+    {
+        public frm_Student_List()
+        {
+            InitializeComponent();
+        }
+        private void btn_Add_Student_Click(object sender, EventArgs e)
+        {
+            frm_Add_New_Student obj = new frm_Add_New_Student();
+            obj.Show();
+            this.Hide();
+        }
+
+        private void btn_Logout_Click(object sender, EventArgs e)
+        {
+            frm_Login obj = new frm_Login();
+            obj.Show();
+            this.Show();
+        }
+    }
+}
