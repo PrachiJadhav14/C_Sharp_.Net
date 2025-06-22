239cf827cdc89bee673335269a10ef469549f57a
PrachiJadhav14 <prachijadhav5514@gmail.com>
2025-06-22 16:42:38 +0530

    Assignment_No_1

--- Windows_Applications/Assignment_No_01/Fork_Addmission_System/Properties/Settings.Designer.cs
+++ Windows_Applications/Assignment_No_01/Fork_Addmission_System/Properties/Settings.Designer.cs
@@ -8,21 +8,40 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Fork_Addmission_System.Properties
-{
+namespace Fork_Addmission_System.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-T2Q4BSUC;Initial Catalog=YC_Student_Addmission_System_DB;Integ" +
+            "rated Security=True")]
+        public string YC_Student_Addmission_System_DBConnectionString {
+            get {
+                return ((string)(this["YC_Student_Addmission_System_DBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=LAPTOP-T2Q4BSUC;Initial Catalog=Fork_Addmission_System;Integrated Sec" +
+            "urity=True")]
+        public string Fork_Addmission_SystemConnectionString {
+            get {
+                return ((string)(this["Fork_Addmission_SystemConnectionString"]));
             }
         }
     }
